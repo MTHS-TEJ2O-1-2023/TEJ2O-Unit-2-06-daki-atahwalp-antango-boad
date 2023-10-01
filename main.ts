f211d4505077453e0af8ef9406c962bc4426c03f
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-09-30 22:01:52 -0400

    Microbit turns on and off an LED

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daki A.B
+ * Created on: Sep 2023
+ * This program turns a single LED light on and off
 */
 
-basic.showString('Hello, World!')
+// On start
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+// turning on LED
+input.onButtonPressed(Button.A, function () {
+  basic.showIcon(IconNames.Yes)
+  pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turning off LED
+input.onButtonPressed(Button.B, function () {
+  basic.showIcon(IconNames.No)
+  pins.digitalWritePin(DigitalPin.P16, 0)
+})
